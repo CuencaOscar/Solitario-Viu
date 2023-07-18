dc5102dea1a21b8990cef12d0f707ae6a7196c94
oscar <ocuencamoreno@gmail.com>
2023-07-18 12:15:12 -0500

    full mazo

--- js/solitario.js
+++ js/solitario.js
@@ -3,9 +3,9 @@
 // Array de palos
 let palos = ["viu", "cua", "hex", "cir"];
 // Array de número de cartas
-// let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 // En las pruebas iniciales solo se trabajará con cuatro cartas por palo:
-let numeros = [9, 10, 11, 12];
+// let numeros = [9, 10, 11, 12];
 // Array ed objetos para definir colores
 let colores = {
   viu: "naranja",
